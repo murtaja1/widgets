bb4c98e66b2ba34f2c5628ab1da8d158b7118c6e
murtaja1 <murtajaadnan7@gmail.com>
2021-09-17 11:14:04 +0300

    Feat: create new widget for copying the text

---
+++ static/src/js/C_CopyClipboardText.js
@@ -0,0 +1,20 @@
+odoo.define('w-test.C-CopyClipboardText', function (require) {
+
+  var fieldRegistry = require('web.field_registry');
+  var AbstractField = require('web.AbstractField');
+  var basic_fields = require('web.basic_fields');
+
+  var C_CopyClipboardText = basic_fields.TextCopyClipboard.extend({
+    template: 'C_CopyClipboardText',
+    events: _.extend({}, AbstractField.prototype.events, {
+      'click .o_clipboard_button': '_onBtnClick'
+    }),
+    _onBtnClick: function (ev) {
+      alert('congrates you just copied: '+ this.value)
+    }
+  })
+
+  fieldRegistry.add('C_CopyClipboardText', C_CopyClipboardText);
+  return C_CopyClipboardText;
+
+})
