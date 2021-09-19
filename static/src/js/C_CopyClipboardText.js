18bb14e69d94943c8a9c13de4220de98d72a8844
murtaja1 <murtajaadnan7@gmail.com>
2021-09-19 11:40:38 +0300

    Fix: comment template name

--- static/src/js/C_CopyClipboardText.js
+++ static/src/js/C_CopyClipboardText.js
@@ -5,7 +5,7 @@ odoo.define('w-test.C-CopyClipboardText', function (require) {
   var basic_fields = require('web.basic_fields');
 
   var C_CopyClipboardText = basic_fields.TextCopyClipboard.extend({
-    template: 'C_CopyClipboardText',
+    // template: 'C_CopyClipboardText',
     events: _.extend({}, AbstractField.prototype.events, {
       'click .o_clipboard_button': '_onBtnClick'
     }),
