2a60f8fba41205743719f1b72d42c4ce8130fbb0
murtaja1 <murtajaadnan7@gmail.com>
2021-09-21 11:17:13 +0300

    Style: added icon and adjust style

--- static/src/js/C_CopyClipboardText.js
+++ static/src/js/C_CopyClipboardText.js
@@ -1,30 +1,28 @@
-odoo.define('w-test.C-CopyClipboardText', function (require) {
+odoo.define("w-test.C-CopyClipboardText", function (require) {
+	var fieldRegistry = require("web.field_registry")
+	var AbstractField = require("web.AbstractField")
+	var basic_fields = require("web.basic_fields")
 
-  var fieldRegistry = require('web.field_registry');
-  var AbstractField = require('web.AbstractField');
-  var basic_fields = require('web.basic_fields');
+	var C_CopyClipboardText = basic_fields.TextCopyClipboard.extend({
+		// template: 'C_CopyClipboardText',
+		events: _.extend({}, AbstractField.prototype.events, {
+			"click .o_clipboard_button": "_onBtnClick"
+		}),
+		_onBtnClick: function (ev) {
+			var divTag = document.createElement("div")
+			divTag.classList.add("copy-text")
+			divTag.innerHTML =
+				"<span>You copied <span class='fa fa-clipboard'></span></span>: <br>" +
+				`<h4>${this.value}</h4>`
 
-  var C_CopyClipboardText = basic_fields.TextCopyClipboard.extend({
-    // template: 'C_CopyClipboardText',
-    events: _.extend({}, AbstractField.prototype.events, {
-      'click .o_clipboard_button': '_onBtnClick'
-    }),
-    _onBtnClick: function (ev) {
+			var element = document.querySelector(".o_clipboard_button")
+			element.appendChild(divTag)
+			setTimeout(function () {
+				element.innerHTML = '<span class="fa fa-clipboard"></span><span> Copy Text</span>'
+			}, 2000)
+		}
+	})
 
-       var tag = document.createElement("div");
-       tag.classList.add('copy-text')
-       tag.innerHTML = ('You copied: <br>'+ `<h4>${this.value}</h4>`);
-
-       var element = document.querySelector(".o_clipboard_button")
-       element.appendChild(tag)
-       setTimeout(function(){
-          element.innerHTML = '<span class="fa fa-clipboard"></span><span> Copy Text</span>'
-        }, 2000)
-       
-    }
-  })
-
-  fieldRegistry.add('C_CopyClipboardText', C_CopyClipboardText);
-  return C_CopyClipboardText;
-
+	fieldRegistry.add("C_CopyClipboardText", C_CopyClipboardText)
+	return C_CopyClipboardText
 })
